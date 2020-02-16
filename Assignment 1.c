fec5be3d7a41e76af24658c2273d66783985adc7
vaibhav2422 <vaibhavgholap4411@gmail.com>
2020-02-16 11:47:42 +0530

    Function modified

--- Assignment 1.c
+++ Assignment 1.c
@@ -1,10 +1,3 @@
-// Problem Statement
-//-------------------------------------------------------------------------------
-// Input a string from user on command line.
-// String may have multiple commas e.g. "Welcome,to,Sunbeam,CDAC,Diploma,Course".
-// Print each word individually. Hint: use strtok() function
-//-------------------------------------------------------------------------------
-
 #include "stdio.h"
 #include "string.h"
 
